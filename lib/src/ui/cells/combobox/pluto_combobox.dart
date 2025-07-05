a4933b4eba709713469387299d4b92f47e451992
MuhammedKasujja <al.kasmud.2@gmail.com>
2025-07-05 11:19:10 +0300

    feat: remove setstate on right value

--- lib/src/ui/cells/combobox/pluto_combobox.dart
+++ lib/src/ui/cells/combobox/pluto_combobox.dart
@@ -191,15 +191,15 @@ class _PlutoComboboxCellState<T extends Object>
 
     _textController.text = value.left ?? '';
 
-    setState(() {
-      selectedOption = value.right;
-    });
+    // setState(() {
+    selectedOption = value.right;
+    // });
 
     _textController.text = formattedValue.left ?? '';
 
-    setState(() {
-      selectedOption = formattedValue.right;
-    });
+    // setState(() {
+    selectedOption = formattedValue.right;
+    // });
 
     _initialCellValue = formattedValue.toJson();
 
